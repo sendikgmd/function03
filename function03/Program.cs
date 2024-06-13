68f184cf0a4ffe60ad2c61a17d92d3323997663a
sendikgmd <163888441+sendikgmd@users.noreply.github.com>
2024-06-13 11:32:31 +0300

    Добавьте файлы проекта.

---
+++ function03/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MyFirstApp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int number = GetNumber();
+            
+            Console.WriteLine("Введенный символ "+ number   +" идентифицирован, как число.");
+            Console.ReadKey();
+        }
+        static int GetNumber()
+        {
+            int number = 0;
+            bool isProgramWork = true;
+
+            while (isProgramWork)
+            {
+                Console.Write("Введите число: ");
+
+                string inputUser = Console.ReadLine();
+
+                if (int.TryParse(inputUser, out number))
+                {
+                    isProgramWork = false;
+                }
+                else
+                {
+                    Console.WriteLine("Введенное значение не является числом.");
+                }
+            }
+            return number;
+        }
+    }
+}
